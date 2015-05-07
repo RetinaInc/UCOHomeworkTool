4a966abf73a256be51fe2a3dc489758ff8ae9201
Steven <steven.chambers@cox.net>
2015-05-07 17:21:54 -0500

    Initial Commit

---
+++ UCOHomeworkTool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UCOHomeworkTool.Startup))]
+namespace UCOHomeworkTool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
