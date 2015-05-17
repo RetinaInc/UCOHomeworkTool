5274d40781bdbaf5c0556efd4837c4865b083661
Steven <steven.chambers@cox.net>
2015-05-17 16:07:14 -0500

    make adjustments to login screen

--- UCOHomeworkTool/Models/AccountViewModels.cs
+++ UCOHomeworkTool/Models/AccountViewModels.cs
@@ -5,7 +5,7 @@ namespace UCOHomeworkTool.Models
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "Student ID")]
         public string UserName { get; set; }
     }
 
@@ -31,7 +31,7 @@ namespace UCOHomeworkTool.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "Student ID")]
         public string UserName { get; set; }
 
         [Required]
